1858ef57ca108e6008f176e8c9201b4777532f20
Ellat717 <97961032+Ellat717@users.noreply.github.com>
2022-01-31 14:15:35 -0500

    Add files via upload

---
+++ map/random.js
@@ -0,0 +1,10 @@
+<script
+function randomUrl() {
+    var randomNumber = Math.round(Math.random())
+    var link1="1.html"
+	var link2="17.html"
+	if (randomnumber==0)
+		window.location=link1
+	else if (randomnumber==1)
+		window.location=link2
+}</script>
